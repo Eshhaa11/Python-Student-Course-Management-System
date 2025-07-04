0aef913cfce3915a5ac4df92f90040ab73e2bc32
Esha Patel <Patellesha11@gmail.com>
2025-07-04 12:31:59 +0300

    Done

--- models/students.py
+++ models/students.py
@@ -3,3 +3,8 @@ from models.base import Base
 
 class Student(Base):
     __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    
