078580098df1261e068ecab447e7a686ecbbd3e6
Esha Patel <Patellesha11@gmail.com>
2025-07-04 12:44:02 +0300

    Done

--- tests/test_enrollment.py
+++ tests/test_enrollment.py
@@ -1,5 +1,5 @@
 import unittest
-from models.student import Student
+from models.students import Student
 from models.course import Course
 from linkedlist.history import CourseHistory
 
@@ -7,13 +7,13 @@ class TestStudentCourseSystem(unittest.TestCase):
 
     def test_student_creation(self):
         student = Student(name="Esha Patel", email="eshaptl1212@gmail.com")
-        self.assertEqual(student.name, "John Doe")
-        self.assertEqual(student.email, "john@example.com")
+        self.assertEqual(student.name, "Esha Patel")
+        self.assertEqual(student.email, "eshaptl1212@gmail.com")
 
     def test_course_creation(self):
-        course = Course(title="Math 101", instructor="Prof. X")
+        course = Course(title="Math 101", instructor="Prof. Henry")
         self.assertEqual(course.title, "Math 101")
-        self.assertEqual(course.instructor, "Prof. X")
+        self.assertEqual(course.instructor, "Prof. Henry")
 
     def test_course_history(self):
         history = CourseHistory()
