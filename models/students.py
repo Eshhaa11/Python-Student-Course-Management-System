c97259e75cdd80e2228d0db6988786da8efde442
Esha Patel <Patellesha11@gmail.com>
2025-07-04 12:34:07 +0300

    Done

--- models/students.py
+++ models/students.py
@@ -7,4 +7,10 @@ class Student(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"<Student(id={self.id},name='{self.name}', email='{self.email}')>"
